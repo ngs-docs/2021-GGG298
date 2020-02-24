bd02c135917252b16bc0f7124599fbbd151bb61b
Shannon EK Joslin <sejoslin@ucdavis.edu>
2020-02-24 13:38:02 -0800

    modify for RNAseq Rmark lesson

--- binder/install.R
+++ binder/install.R
@@ -5,4 +5,5 @@ BiocManager::install("edgeR", update=FALSE, ask=FALSE)
 
 install.packages(c("highr", "markdown", "stringr", "yaml"))
 install.packages(c("caTools", "bitops", "knitr", "rprojroot", "rmarkdown"))
+install.packages(c("ggplot2", "RColorBrewer", "edgeR", "gplots", "GGally"))
 tinytex::install_tinytex()
